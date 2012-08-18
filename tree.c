fb144ef055f6a1c81b05d54958b9c9accd020069
baskerville <nihilhill@gmail.com>
2012-08-18 14:55:16 +0200

    Find neighbor

--- tree.c
+++ tree.c
@@ -22,19 +22,34 @@ Node *second_extrema(Node *n)
 
 Node *find_neighbor(Node *n, direction_t dir)
 {
-    if (n == NULL)
+    Node *p = n->parent;
+    Node *neighbor = NULL;
+
+    if (n == NULL || p == NULL)
         return NULL;
-    switch (dir) {
-        case DIR_UP:
-            break;
-        case DIR_LEFT:
-            break;
-        case DIR_DOWN:
-            break;
-        case DIR_RIGHT:
-            break;
+
+    while (p != NULL && neighbor == NULL) {
+        switch (dir) {
+            case DIR_UP:
+                if (p->split_type == TYPE_HORIZONTAL && p->rectangle.y < n->rectangle.y)
+                    neighbor = second_extrema(p->first_child);
+                break;
+            case DIR_LEFT:
+                if (p->split_type == TYPE_VERTICAL && p->rectangle.x < n->rectangle.x)
+                    neighbor = second_extrema(p->first_child);
+                break;
+            case DIR_DOWN:
+                if (p->split_type == TYPE_HORIZONTAL && (p->rectangle.y + p->rectangle.height) > (n->rectangle.y + n->rectangle.height))
+                    neighbor = first_extrema(p->second_child);
+                break;
+            case DIR_RIGHT:
+                if (p->split_type == TYPE_VERTICAL && (p->rectangle.x + p->rectangle.width) > (n->rectangle.x + n->rectangle.width))
+                    neighbor = first_extrema(p->second_child);
+                break;
+        }
+        p = p->parent;
     }
-    return NULL;
+    return neighbor;
 }
 
 void rotate_tree(Node *n, rotate_t rot)
