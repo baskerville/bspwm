55d347c6acf6a4b976814756c4e0254007cc3d4f
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-10-03 20:07:08 +0300

    update client to use helpers

--- bspc.c
+++ bspc.c
@@ -1,31 +1,10 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include "helpers.h"
 #include "common.h"
-
-static void logmsg(FILE *stream, char *fmt, va_list ap) {
-    vfprintf(stream, fmt, ap);
-}
-
-static void warn(char *warnfmt, ...) {
-    va_list ap;
-    va_start(ap, warnfmt);
-    logmsg(stderr, warnfmt, ap);
-    va_end(ap);
-}
-
-__attribute__((noreturn))
-static void err(char *errfmt, ...) {
-    va_list ap;
-    va_start(ap, errfmt);
-    logmsg(stderr, errfmt, ap);
-    va_end(ap);
-    exit(EXIT_FAILURE);
-}
 
 int main(int argc, char *argv[])
 {
@@ -36,13 +15,13 @@ int main(int argc, char *argv[])
     char rsp[BUFSIZ];
 
     if (argc < 2)
-        err("no arguments given\n");
+        die("no arguments given\n");
 
     char *sock_path = getenv(SOCKET_ENV_VAR);
     if (sock_path == NULL || strlen(sock_path) == 0)
         warn("environmental variable '%s' is not set or empty - using default value: %s\n", SOCKET_ENV_VAR, DEFAULT_SOCKET_PATH);
     else if (sizeof(sock_address.sun_path) <= strlen(sock_path))
-        err("value too long for environmental variable '%s'\n", SOCKET_ENV_VAR);
+        die("value too long for environmental variable '%s'\n", SOCKET_ENV_VAR);
 
     sock_address.sun_family = AF_UNIX;
     strncpy(sock_address.sun_path, (sock_path == NULL ? DEFAULT_SOCKET_PATH: sock_path), sizeof(sock_address.sun_path));
@@ -57,17 +36,17 @@ int main(int argc, char *argv[])
 
     sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sock_fd == -1)
-        err("failed to create socket\n");
+        die("failed to create socket\n");
 
     if (connect(sock_fd, (struct sockaddr *) &sock_address, sizeof(sock_address)) == -1)
-        err("failed to connect to socket\n");
+        die("failed to connect to socket\n");
 
     if (send(sock_fd, msg, msglen, 0) == -1)
-        err("failed to send data\n");
+        die("failed to send data\n");
 
     int n = recv(sock_fd, rsp, sizeof(rsp), 0);
     if (n == -1)
-        err("failed to get response\n");
+        die("failed to get response\n");
     else if (n > 0) {
         rsp[n] = '\0';
         printf("%s\n", rsp);
