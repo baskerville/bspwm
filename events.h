2400ce9e6b03fdd76d018eea4029667c0d7a772d
Bastien Dejean <nihilhill@gmail.com>
2013-01-09 16:47:20 +0100

    Remove obsolete prototypes

--- events.h
+++ events.h
@@ -11,11 +11,10 @@ void unmap_notify(xcb_generic_event_t *);
 void configure_request(xcb_generic_event_t *);
 void client_message(xcb_generic_event_t *);
 void property_notify(xcb_generic_event_t *);
-void mapping_notify(xcb_generic_event_t *);
-void enter_notify(xcb_generic_event_t *);
 void motion_notify(xcb_generic_event_t *);
 void handle_state(monitor_t *, desktop_t *, node_t *, xcb_atom_t, unsigned int);
 void grab_pointer(pointer_action_t);
 void track_pointer(int, int);
 void ungrab_pointer(void);
+
 #endif
