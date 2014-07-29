1e0a7c826e5f82e31b18bf4c6173add2ea9306bd
Bastien Dejean <nihilhill@gmail.com>
2014-07-29 23:33:23 +0200

    Fix regression introduced by aebb671

--- subscribe.c
+++ subscribe.c
@@ -87,7 +87,8 @@ int print_status(FILE *stream)
 				c = toupper(c);
 			fprintf(stream, "%c%s:", c, d->name);
 		}
-		fprintf(stream, "L%c%s", (m->desk->layout == LAYOUT_TILED ? 'T' : 'M'), (m != mon_tail ? ":" : ""));
+		if (m->desk != NULL)
+			fprintf(stream, "L%c%s", (m->desk->layout == LAYOUT_TILED ? 'T' : 'M'), (m != mon_tail ? ":" : ""));
 	}
 	fprintf(stream, "%s", "\n");
 	return fflush(stream);
