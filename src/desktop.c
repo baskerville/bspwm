baskerville/bspwm

commit 875defe46997b983bbb3102567bdd68866c202bb

Properly handle sticky nodes in transfer_desktop
Fixes #865.
Closes #852.

src/desktop.c
@@ -74,8 +74,8 @@ bool activate_desktop(monitor_t *m, desktop_t *d)
 		return false;
 	}
 
-	if (m->sticky_count > 0) {
-		transfer_sticky_nodes(m, m->desk, d, m->desk->root);
+	if (m->sticky_count > 0 && m->desk != NULL) {
+		transfer_sticky_nodes(m, m->desk, m, d, m->desk->root);
 	}
 
 	show_desktop(d);
@@ -180,7 +180,9 @@ bool transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d, bool follow)
 	unlink_desktop(ms, d);
 
 	if ((!follow || !d_was_active || !ms_was_focused) && md->desk != NULL) {
+		hide_sticky = false;
 		hide_desktop(d);
+		hide_sticky = true;
 	}
 
 	insert_desktop(md, d);
@@ -204,7 +206,15 @@ bool transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d, bool follow)
 	}
 
 	if (ms->sticky_count > 0 && d_was_active) {
-		transfer_sticky_nodes(ms, d, ms->desk, d->root);
+		if (ms->desk != NULL) {
+			transfer_sticky_nodes(md, d, ms, ms->desk, d->root);
+		} else {
+			ms->sticky_count -= sticky_count(d->root);
+			md->sticky_count += sticky_count(d->root);
+			if (d != md->desk) {
+				transfer_sticky_nodes(md, d, md, md->desk, d->root);
+			}
+		}
 	}
 
 	adapt_geometry(&ms->rectangle, &md->rectangle, d->root);
@@ -359,6 +369,7 @@ void merge_desktops(monitor_t *ms, desktop_t *ds, monitor_t *md, desktop_t *dd)
 	if (ds == NULL || dd == NULL || ds == dd) {
 		return;
 	}
+	/* TODO: Handle sticky nodes. */
 	transfer_node(ms, ds, ds->root, md, dd, dd->focus, false);
 }
 

src/tree.c
@@ -493,21 +493,21 @@ bool activate_node(monitor_t *m, desktop_t *d, node_t *n)
 	return true;
 }
 
-void transfer_sticky_nodes(monitor_t *m, desktop_t *ds, desktop_t *dd, node_t *n)
+void transfer_sticky_nodes(monitor_t *ms, desktop_t *ds, monitor_t *md, desktop_t *dd, node_t *n)
 {
 	if (n == NULL) {
 		return;
 	} else if (n->sticky) {
 		sticky_still = false;
-		transfer_node(m, ds, n, m, dd, dd->focus, false);
+		transfer_node(ms, ds, n, md, dd, dd->focus, false);
 		sticky_still = true;
 	} else {
 		/* we need references to the children because n might be freed after
 		 * the first recursive call */
 		node_t *first_child = n->first_child;
 		node_t *second_child = n->second_child;
-		transfer_sticky_nodes(m, ds, dd, first_child);
-		transfer_sticky_nodes(m, ds, dd, second_child);
+		transfer_sticky_nodes(ms, ds, md, dd, first_child);
+		transfer_sticky_nodes(ms, ds, md, dd, second_child);
 	}
 }
 
@@ -561,12 +561,12 @@ bool focus_node(monitor_t *m, desktop_t *d, node_t *n)
 		clear_input_focus();
 	}
 
-	if (m->sticky_count > 0 && d != m->desk) {
+	if (m->sticky_count > 0 && m->desk != NULL && d != m->desk) {
 		if (guess && m->desk->focus != NULL && m->desk->focus->sticky) {
 			n = m->desk->focus;
 		}
 
-		transfer_sticky_nodes(m, m->desk, d, m->desk->root);
+		transfer_sticky_nodes(m, m->desk, m, d, m->desk->root);
 
 		if (n == NULL && d->focus != NULL) {
 			n = d->focus;
@@ -636,7 +636,7 @@ bool focus_node(monitor_t *m, desktop_t *d, node_t *n)
 
 void hide_node(desktop_t *d, node_t *n)
 {
-	if (n == NULL) {
+	if (n == NULL || (!hide_sticky && n->sticky)) {
 		return;
 	} else {
 		if (!n->hidden) {
@@ -1445,11 +1445,6 @@ bool transfer_node(monitor_t *ms, desktop_t *ds, node_t *ns, monitor_t *md, desk
 	if (md != ms) {
 		if (ns->client == NULL || monitor_from_client(ns->client) != md) {
 			adapt_geometry(&ms->rectangle, &md->rectangle, ns);
-		}
-
-		if (ms->sticky_count > 0 && sticky_count(ns) > 0) {
-			ms->sticky_count -= sticky_count(ns);
-			md->sticky_count += sticky_count(ns);
 		}
 	}
 

src/bspwm.c
@@ -215,7 +215,7 @@ void init(void)
 	stack_head = stack_tail = NULL;
 	subscribe_head = subscribe_tail = NULL;
 	pending_rule_head = pending_rule_tail = NULL;
-	auto_raise = sticky_still = record_history = true;
+	auto_raise = sticky_still = hide_sticky = record_history = true;
 	auto_split_type = false;
 	randr_base = 0;
 	exit_status = 0;

src/bspwm.h
@@ -79,6 +79,7 @@ int exit_status;
 bool auto_raise;
 bool auto_split_type;
 bool sticky_still;
+bool hide_sticky;
 bool record_history;
 bool running;
 bool randr;

src/tree.h
@@ -40,7 +40,7 @@ node_t *find_public(desktop_t *d);
 node_t *insert_node(monitor_t *m, desktop_t *d, node_t *n, node_t *f);
 void insert_receptacle(monitor_t *m, desktop_t *d, node_t *n);
 bool activate_node(monitor_t *m, desktop_t *d, node_t *n);
-void transfer_sticky_nodes(monitor_t *m, desktop_t *ds, desktop_t *dd, node_t *n);
+void transfer_sticky_nodes(monitor_t *ms, desktop_t *ds, monitor_t *md, desktop_t *dd, node_t *n);
 bool focus_node(monitor_t *m, desktop_t *d, node_t *n);
 void hide_node(desktop_t *d, node_t *n);
 void show_node(desktop_t *d, node_t *n);

commit a827412f1de9eb409584d3891f91803794d8d61c

Properly update the sticky count in `transfer_*`
I think the initial regression came from
875defe46997b983bbb3102567bdd68866c202bb (the removed lines at the end)
which led to #1098.

This issue was fixed by 5df311b12c06dca86f8e5a56ee522bd48d2178bc. This
commit brought another regression which led to #1199.

Fixes #1199.

src/desktop.c
@@ -172,8 +172,10 @@ bool transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d, bool follow)
 
 	bool d_was_active = (d == ms->desk);
 	bool ms_was_focused = (ms == mon);
+	unsigned int sc = (ms->sticky_count > 0 && d_was_active) ? sticky_count(d->root) : 0;
 
 	unlink_desktop(ms, d);
+	ms->sticky_count -= sc;
 
 	if ((!follow || !d_was_active || !ms_was_focused) && md->desk != NULL) {
 		hide_sticky = false;
@@ -182,6 +184,7 @@ bool transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d, bool follow)
 	}
 
 	insert_desktop(md, d);
+	md->sticky_count += sc;
 	history_remove(d, NULL, false);
 
 	if (d_was_active) {
@@ -201,15 +204,11 @@ bool transfer_desktop(monitor_t *ms, monitor_t *md, desktop_t *d, bool follow)
 		}
 	}
 
-	if (ms->sticky_count > 0 && d_was_active) {
+	if (sc > 0) {
 		if (ms->desk != NULL) {
 			transfer_sticky_nodes(md, d, ms, ms->desk, d->root);
-		} else {
-			ms->sticky_count -= sticky_count(d->root);
-			md->sticky_count += sticky_count(d->root);
-			if (d != md->desk) {
-				transfer_sticky_nodes(md, d, md, md->desk, d->root);
-			}
+		} else if (d != md->desk) {
+			transfer_sticky_nodes(md, d, md, md->desk, d->root);
 		}
 	}
 

src/tree.c
@@ -433,8 +433,6 @@ node_t *insert_node(monitor_t *m, desktop_t *d, node_t *n, node_t *f)
 		}
 	}
 
-	m->sticky_count += sticky_count(n);
-
 	propagate_flags_upward(m, d, n);
 
 	if (d->focus == NULL && is_focusable(n)) {
@@ -1312,10 +1310,6 @@ void unlink_node(monitor_t *m, desktop_t *d, node_t *n)
 
 	node_t *p = n->parent;
 
-	if (m->sticky_count > 0) {
-		m->sticky_count -= sticky_count(n);
-	}
-
 	if (p == NULL) {
 		d->root = NULL;
 		d->focus = NULL;
@@ -1418,6 +1412,9 @@ void remove_node(monitor_t *m, desktop_t *d, node_t *n)
 	history_remove(d, n, true);
 	remove_stack_node(n);
 	cancel_presel_in(m, d, n);
+	if (m->sticky_count > 0 && d == m->desk) {
+		m->sticky_count -= sticky_count(n);
+	}
 	clients_count -= clients_count_in(n);
 	if (is_descendant(grabbed_node, n)) {
 		grabbed_node = NULL;
@@ -1599,7 +1596,8 @@ bool transfer_node(monitor_t *ms, desktop_t *ds, node_t *ns, monitor_t *md, desk
 		return false;
 	}
 
-	if (sticky_still && ms->sticky_count > 0 && sticky_count(ns) > 0 && dd != md->desk) {
+	unsigned int sc = (ms->sticky_count > 0 && ds == ms->desk) ? sticky_count(ns) : 0;
+	if (sticky_still && sc > 0 && dd != md->desk) {
 		return false;
 	}
 
@@ -1621,6 +1619,8 @@ bool transfer_node(monitor_t *ms, desktop_t *ds, node_t *ns, monitor_t *md, desk
 		if (ns->client == NULL || monitor_from_client(ns->client) != md) {
 			adapt_geometry(&ms->rectangle, &md->rectangle, ns);
 		}
+		ms->sticky_count -= sc;
+		md->sticky_count += sc;
 	}
 
 	if (ds != dd) {