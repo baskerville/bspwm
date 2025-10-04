baskerville/bspwm

commit 6082d8b6fcc9bc3e69be198fe60a80e061063fcc

Revamp signal handling
- Change from signal to sigaction, which is more portable.

- Install a handler for SIGPIPE instead of ignoring it, to avoid
  passing on the ignored status to other programs launched from
  bspwm.

- Use SA_NOCLDWAIT to avoid the need to call waitpid. (NB if waitpid
  were called in the handler, it would be a good idea to protect
  errno.)

- Declare the "running" global as volatile sig_atomic_t, as recommended
  for values modified from signal handlers.

Closes #1480.

src/bspwm.c
@@ -27,7 +27,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
-#include <sys/wait.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <fcntl.h>
@@ -87,7 +86,7 @@ bool auto_raise;
 bool sticky_still;
 bool hide_sticky;
 bool record_history;
-bool running;
+volatile sig_atomic_t running;
 bool restart;
 bool randr;
 
@@ -104,6 +103,7 @@ int main(int argc, char *argv[])
 	xcb_generic_event_t *event;
 	char *end;
 	int opt;
+	struct sigaction sigact;
 
 	while ((opt = getopt(argc, argv, "hvc:s:o:")) != -1) {
 		switch (opt) {
@@ -194,11 +194,20 @@ int main(int argc, char *argv[])
 
 	fcntl(sock_fd, F_SETFD, FD_CLOEXEC | fcntl(sock_fd, F_GETFD));
 
-	signal(SIGINT, sig_handler);
-	signal(SIGHUP, sig_handler);
-	signal(SIGTERM, sig_handler);
-	signal(SIGCHLD, sig_handler);
-	signal(SIGPIPE, SIG_IGN);
+	sigact.sa_handler = sig_handler;
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sigact, NULL);
+	sigaction(SIGHUP, &sigact, NULL);
+	sigaction(SIGTERM, &sigact, NULL);
+	/* We avoid using SIG_IGN with SIGPIPE because that would be preserved across
+	   exec. */
+	sigaction(SIGPIPE, &sigact, NULL);
+
+	sigact.sa_handler = SIG_IGN;
+	sigact.sa_flags = SA_NOCLDWAIT;
+	sigaction(SIGCHLD, &sigact, NULL);
+
 	run_config(run_level);
 	running = true;
 
@@ -527,11 +536,7 @@ bool check_connection (xcb_connection_t *dpy)
 
 void sig_handler(int sig)
 {
-	if (sig == SIGCHLD) {
-		signal(sig, sig_handler);
-		while (waitpid(-1, 0, WNOHANG) > 0)
-			;
-	} else if (sig == SIGINT || sig == SIGHUP || sig == SIGTERM) {
+	if (sig == SIGINT || sig == SIGHUP || sig == SIGTERM) {
 		running = false;
 	}
 }

src/bspwm.h
@@ -25,6 +25,7 @@
 #ifndef BSPWM_BSPWM_H
 #define BSPWM_BSPWM_H
 
+#include <signal.h>
 #include "types.h"
 
 #define WM_NAME                  "bspwm"
@@ -84,7 +85,7 @@ extern bool auto_raise;
 extern bool sticky_still;
 extern bool hide_sticky;
 extern bool record_history;
-extern bool running;
+extern volatile sig_atomic_t running;
 extern bool restart;
 extern bool randr;
 

commit 66294f49975ecba70c55207a373ecef5aac79fbb

Handle SIGCHLD instead of ignoring it.
On Linux, if SIGCHLD is ignored, then it is still ignored after execve,
and ignoring SIGCHLD caused spawned processes to not be able to see
child statuses with waitpid.

This fixes a problem introduced with commit 6082d8b6, which I had only
tested on OpenBSD, where execve resets the ignored status of SIGCHLD.

src/bspwm.c
@@ -34,6 +34,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <xcb/xinerama.h>
 #include <xcb/xcb_aux.h>
 #include "types.h"
@@ -197,16 +198,13 @@ int main(int argc, char *argv[])
 	sigact.sa_handler = sig_handler;
 	sigemptyset(&sigact.sa_mask);
 	sigact.sa_flags = SA_RESTART;
+	sigaction(SIGCHLD, &sigact, NULL);
 	sigaction(SIGINT, &sigact, NULL);
 	sigaction(SIGHUP, &sigact, NULL);
 	sigaction(SIGTERM, &sigact, NULL);
 	/* We avoid using SIG_IGN with SIGPIPE because that would be preserved across
 	   exec. */
 	sigaction(SIGPIPE, &sigact, NULL);
-
-	sigact.sa_handler = SIG_IGN;
-	sigact.sa_flags = SA_NOCLDWAIT;
-	sigaction(SIGCHLD, &sigact, NULL);
 
 	run_config(run_level);
 	running = true;
@@ -536,7 +534,10 @@ bool check_connection (xcb_connection_t *dpy)
 
 void sig_handler(int sig)
 {
-	if (sig == SIGINT || sig == SIGHUP || sig == SIGTERM) {
+	if (sig == SIGCHLD) {
+		while (waitpid(-1, 0, WNOHANG) > 0)
+			;
+	} else if (sig == SIGINT || sig == SIGHUP || sig == SIGTERM) {
 		running = false;
 	}
 }